b025bfa173d671e89992ea9817589ce2eadb1d4a
badrinaran944-dev <badrinaran944@gmail.com>
2026-06-22 15:13:22 +0530

    Update generate-names.js

--- api/generate-names.js
+++ api/generate-names.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Sirf POST request allow karo
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
